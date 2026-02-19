5c7bcbcab4031c9619adaab9b25e30d0799ec297
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2026-02-19 21:38:30 +0300

    chore: bootstrap project deps and dockerized postgres

---
+++ document-management-service/src/test/java/com/nbenliogludev/documentmanagementservice/DocumentManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbenliogludev.documentmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
